e9122cd2b483ee08924b2bd6fad2ee12f3a86bf2
unknown <mohammadalmobayed4@gmail.com>
2022-11-06 20:11:06 +0200

    r

---
+++ Sun_6_nov/task1.js
@@ -0,0 +1,20 @@
+let x = prompt('enter your birthday month ')
+
+// if ( x ==😎
+// {
+//   console.log( 'August ');
+// }
+//  else if ( x ==3)
+// {
+//   console.log( 'March');
+// }
+
+switch(x)
+{
+    case '8':
+    console.log( 'August ')
+    break;
+    case '3':
+    console.log( 'march ')
+    break;
+}
