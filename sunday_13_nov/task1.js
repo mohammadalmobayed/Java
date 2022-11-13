a32a967be6a111141b531e4addc5c0c742c4a250
unknown <mohammadalmobayed4@gmail.com>
2022-11-13 23:51:51 +0200

    f

---
+++ sunday_13_nov/task1.js
@@ -0,0 +1,82 @@
+// let mohammad=[2,4,100];
+// let doubleNumbers= mohammad.map(function(element){
+//     return element*2
+// });
+// console.log(doubleNumbers)
+
+
+// /*task 2*/
+// let mohammad1=[2,4,100];
+//  let me = mohammad1.map(function(element){
+//     return element.toString();
+
+//  })
+// // console.log(me);
+
+/*task3*/
+// var arrayy= ["john", "JACOB", "jinGleHeimer", "schmidt"]
+// let capitalizeNames=arrayy.map((c)  => c[0].toUpperCase()+ c.slice(1))
+// console.log(capitalizeNames)   
+  
+// /*task4*/
+
+// function namesOnly(array) {
+
+
+//     return array.map(i => i.name);
+//     }
+//     var names = namesOnly([
+//     {
+//         name: "Angelina Jolie",
+//         age: 80
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5
+//     },
+//     {
+//         name: "Kayne West",
+//         age: 16
+//     },
+//     {
+//         name: "Bob Ziroll",
+//         age: 100
+//     }
+//     ])
+    
+//     console.log(names);
+
+//     /*task7*/
+// var myArray=[1,-2,-3]
+
+// let doubleValues=myArray.map ((d) => d*2    )
+// console.log(doubleValues)
+
+// /*task8*/
+
+// var myrray=[1,-2,-3]
+
+// let valTimesIndex=myrray.map ((d,i) => d* i )
+// console.log(valTimesIndex)
+
+/*task9*/
+// function extractKey(name_2) {
+
+
+//     return name_2.map(i => i.name);
+//     }
+//     let result = extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]) 
+    
+//     console.log(result)
+
+// /*task10*/
+// var arr = [1, 8 ,4,6];
+
+// var r = arr.filter(function (x) {
+//     return x % 2 == 0;
+// });
+// console.log(r)
---
+++ sunday_13_nov/task2.js
@@ -0,0 +1,30 @@
+// function doubleValues(arr){
+//     const newArr = [];
+//     arr.forEach((val) => {
+//         newArr.push(val * 2);
+//     });
+//     return newArr;
+// }
+// console.log(doubleValues([1,2,3,4]));
+
+
+/*TASK2*/
+
+// function onlyEvenValues(arr){
+//     const arr2 = [];
+//       arr.forEach((val) => {
+//         if(val % 2 === 0) arr2.push(val);
+//       });
+//       return arr2;
+//   }
+//   console.log(onlyEvenValues([1,2,3,4]))
+
+
+
+  /*TASK3*/
+  showFirstAndLast(['colt','matt', 'tim', 'udemy']) // ["ct", "mt", "tm", "uy"]
+    showFirstAndLast(['hi', 'goodbye', 'smile']) // ['hi', 'ge', 'se']
+
+function showFirstAndLast(arr){
+    
+}
